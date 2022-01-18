ae235e2bf428bc42873f5d7a343503f4214f7803
andrew <andrew.dickens@manifesto.co.uk>
2022-01-18 19:28:56 +0000

    Add new fields - text-color, bg-img, darken filter

--- wp-content/themes/rethink-event/template-parts/hero.php
+++ wp-content/themes/rethink-event/template-parts/hero.php
@@ -1,18 +1,40 @@
 <?php $post_id = $args['post_id'];?>
 
-<div class="t-hero t-hero--<?php echo the_field('background_color', $post_id); ?>">
-    <div class="content-layout">
 
+
+<div class="t-hero">
+
+    <div class="t-hero__background  t-hero__background--<?php echo the_field('background_color', $post_id); ?>"></div>
+
+
+    <div
+        class="t-hero__background-image <?php echo get_field('dark_filter') ? 't-hero__background-image--darken' : "" ?>">
+
+
+        <?php
+$image = get_field('background_image');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+if ($image) {
+    echo wp_get_attachment_image($image, $size);
+}
+?>
+    </div>
+
+    <div class="content-layout">
         <!-- check if title is from ACF or post -->
         <?php if (get_field('title', $post_id)): ?>
-        <h1><?php the_field('title', $post_id);?></h1>
-        <?php else: ?>
-        <h1><?php the_title();?></h1>
-        <?php endif;?>
 
-        <?php if (get_field('subtitle', $post_id)): ?>
-        <h3><?php the_field('subtitle', $post_id);?></h3>
-        <?php endif?>
 
+        <div class="t-hero__text t-hero__text--<?php echo the_field('text_color', $post_id); ?>">
+            <h1><?php the_field('title', $post_id);?></h1>
+            <?php else: ?>
+            <h1><?php the_title();?></h1>
+            <?php endif;?>
+
+            <?php if (get_field('subtitle', $post_id)): ?>
+            <h3><?php the_field('subtitle', $post_id);?></h3>
+            <?php endif?>
+        </div>
     </div>
+
 </div>
