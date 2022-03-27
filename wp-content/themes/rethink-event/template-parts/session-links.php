1d4718f9d775983e8e490e649094b3b089960741
andrew <andrew.dickens@manifesto.co.uk>
2022-03-27 16:35:30 +0100

    updates to single profiles style and ACF

--- wp-content/themes/rethink-event/template-parts/session-links.php
+++ wp-content/themes/rethink-event/template-parts/session-links.php
@@ -4,79 +4,79 @@ $post_type = get_post_type($post_id);
 $day1sessions = [];
 $day2sessions = [];
 $sessions = get_posts(array(
-    'post_type' => 'session',
-    'meta_query' => array(
-        'relation' => 'OR',
-        array(
-            'key' => 'speakers',
-            'value' => '"' . $post_id . '"',
-            'compare' => 'LIKE',
-        ),
-        array(
-            'key' => 'moderators',
-            'value' => '"' . $post_id . '"',
-            'compare' => 'LIKE',
-        ),
-        array(
-            'key' => 'partners',
-            'value' => '"' . $post_id . '"',
-            'compare' => 'LIKE',
-        ),
-        array(
-            'key' => 'sponsors',
-            'value' => '"' . $post_id . '"',
-            'compare' => 'LIKE',
-        ),
+  'post_type' => 'session-2022',
+  'meta_query' => array(
+    'relation' => 'OR',
+    array(
+      'key' => 'speakers',
+      'value' => '"' . $post_id . '"',
+      'compare' => 'LIKE',
     ),
+    array(
+      'key' => 'moderators',
+      'value' => '"' . $post_id . '"',
+      'compare' => 'LIKE',
+    ),
+    array(
+      'key' => 'partners',
+      'value' => '"' . $post_id . '"',
+      'compare' => 'LIKE',
+    ),
+    array(
+      'key' => 'sponsors',
+      'value' => '"' . $post_id . '"',
+      'compare' => 'LIKE',
+    ),
+  ),
 ));
 ?>
 
 <?php if ($sessions) : ?>
-    <div class="t-session-links">
+  <div class="t-session-links">
 
-        <?php if ($post_type === 'speaker-items') : ?>
-            <h3>Speaking at:</h3>
-        <?php elseif ($post_type === 'partner-items') : ?>
-            <h3>Supporting:</h3>
-        <?php elseif ($post_type === 'sponsor-items') : ?>
-            <h3>Sponsoring:</h3>
-        <?php endif ?>
+    <?php if ($post_type === 'speaker-items') : ?>
+      <h3>Speaking at:</h3>
+    <?php elseif ($post_type === 'partner-items') : ?>
+      <h3>Supporting:</h3>
+    <?php elseif ($post_type === 'sponsor-items') : ?>
+      <h3>Sponsoring:</h3>
+    <?php endif ?>
 
 
-        <?php foreach ($sessions as $session) : ?>
+    <?php foreach ($sessions as $session) : ?>
 
-            <?php $day = get_field('day', $session->ID); ?>
+      <?php $day = get_field('day', $session->ID); ?>
 
-            <!-- split sessions into two arrays day1/day2 -->
-            <?php
-            if ($day === 'day1') {
-                array_push($day1sessions, $session);
-            } elseif ($day === 'day2') {
+      <!-- split sessions into two arrays day1/day2 -->
+      <?php
+      if ($day === 'day1') {
+        array_push($day1sessions, $session);
+      } elseif ($day === 'day2') {
 
-                array_push($day2sessions, $session);
-            }
-            ?>
-        <?php endforeach; ?>
-        <!-- loop through day 1 sessions -->
-        <?php if ($day1sessions) : ?>
-            <h4>Day 1</h4>
-        <?php endif ?>
+        array_push($day2sessions, $session);
+      }
+      ?>
+    <?php endforeach; ?>
+    <!-- loop through day 1 sessions -->
+    <?php if ($day1sessions) : ?>
+      <h4>Day 1</h4>
+    <?php endif ?>
 
-        <?php foreach ($day1sessions as $session) : ?>
-            <a class="t-session-link" href="<?php echo get_permalink($session->ID); ?>">
-                <?php echo $session->post_title ?>
-            </a>
-        <?php endforeach; ?>
+    <?php foreach ($day1sessions as $session) : ?>
+      <a class="t-session-link" href="<?php echo get_permalink($session->ID); ?>">
+        <?php echo $session->post_title ?>
+      </a>
+    <?php endforeach; ?>
 
-        <!-- loop through day 2 sessions -->
-        <?php if ($day2sessions) : ?>
-            <h4>Day 2</h4>
-        <?php endif ?>
-        <?php foreach ($day2sessions as $session) : ?>
-            <a class="t-session-link" href="<?php echo get_permalink($session->ID); ?>">
-                <?php echo $session->post_title ?>
-            </a> <br>
-        <?php endforeach; ?>
+    <!-- loop through day 2 sessions -->
+    <?php if ($day2sessions) : ?>
+      <h4>Day 2</h4>
+    <?php endif ?>
+    <?php foreach ($day2sessions as $session) : ?>
+      <a class="t-session-link" href="<?php echo get_permalink($session->ID); ?>">
+        <?php echo $session->post_title ?>
+      </a> <br>
+    <?php endforeach; ?>
 
-    </div>
+  </div>
 <?php endif; ?>
--- wp-content/themes/rethink-event/template-parts/speakers.php
+++ wp-content/themes/rethink-event/template-parts/speakers.php
@@ -1,38 +1,38 @@
-<?php $speakers = $args['data']?>
+<?php $speakers = $args['data'] ?>
 
 <div class="t-speakers">
-    <?php foreach ($speakers as $speaker): ?>
+  <?php foreach ($speakers as $speaker) : ?>
 
-    <?php $post_id = $speaker->ID;?>
+    <?php $post_id = $speaker->ID; ?>
 
-    <?php $link = get_permalink($speaker->ID);?>
-    <?php $image = get_field('image', $post_id);?>
-    <?php $moderator = $speaker->moderator?>
+    <?php $link = get_permalink($speaker->ID); ?>
+    <?php $image = get_field('image', $post_id); ?>
+    <?php $moderator = $speaker->moderator ?>
 
     <div class="t-speaker  t-modal-parent <?php echo $moderator ? 't-speaker--moderator' : '' ?>  ">
 
-        <div class="t-speaker__img">
-            <?php if ($image): ?>
-            <?php echo wp_get_attachment_image($image, 'medium'); ?>
-            <?php endif;?>
-        </div>
+      <div class="t-speaker__img">
+        <?php if ($image) : ?>
+          <?php echo wp_get_attachment_image($image, 'medium'); ?>
+        <?php endif; ?>
+      </div>
 
-        <div class="t-speaker__text">
-            <?php if ($moderator): ?> <p class="t-speaker__text__mod">MODERATOR
-            </p>
-            <?php endif?>
+      <div class="t-speaker__text">
+        <?php if ($moderator) : ?> <p class="t-speaker__text__mod">MODERATOR
+          </p>
+        <?php endif ?>
 
-            <p class="t-speaker__text__title">
-                <?php echo get_the_title($speaker->ID); ?>
-            </p>
+        <p class="t-speaker__text__title">
+          <?php echo get_the_title($speaker->ID); ?>
+        </p>
 
-            <p class="t-speaker__text__position"> <?php echo get_field('position', $speaker->ID); ?></p>
-            <p class="t-speaker__text__company"> <?php echo get_field('company', $speaker->ID); ?></p>
-        </div>
+        <p class="t-speaker__text__position"> <?php echo get_field('position', $speaker->ID); ?></p>
+        <p class="t-speaker__text__company"> <?php echo get_field('company', $speaker->ID); ?></p>
+      </div>
 
     </div>
 
-    <?php get_template_part('template-parts/single-modal', null, array('post_id' => $post_id))?>
+    <?php get_template_part('template-parts/single-modal', null, array('post_id' => $post_id)) ?>
 
-    <?php endforeach;?>
+  <?php endforeach; ?>
 </div>
